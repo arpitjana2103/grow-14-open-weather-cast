255b1cc07b8dcac01a5ff84574aef817bf2089b3
arpit jana <arpitjana2103@gmail.com>
2026-05-19 11:14:11 +0530

    feat : useClickoutSide hook created

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,26 @@
+import { useEffect, type RefObject } from "react";
+
+type UseClickOutsideProps = {
+    ref: RefObject<HTMLDivElement | null>;
+    callback: () => void;
+    enabled: boolean;
+};
+
+export function useClickOutside({ ref, callback, enabled }: UseClickOutsideProps) {
+    useEffect(
+        function () {
+            if (!enabled) return;
+
+            function handleClick(e: MouseEvent) {
+                if (ref.current && !ref.current.contains(e.target as Node)) callback();
+            }
+
+            document.addEventListener("mousedown", handleClick);
+
+            return function () {
+                document.removeEventListener("mousedown", handleClick);
+            };
+        },
+        [enabled, ref, callback],
+    );
+}
